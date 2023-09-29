dc9a85281e7981f87b69de9414aacda6d55d24c6
蔡文姬 <2735103940@qq.com>
2023-09-29 15:39:48 +0800

    ing

--- src/views/platform/content/upload/video.vue
+++ src/views/platform/content/upload/video.vue
@@ -51,56 +51,98 @@
         </div>
         <div class="form">
             <ul class="videos">
-                <li
-                v-for="index in 10"
-                >
-                   <div class="icon">
-                    <i class="colourless bofangshu"></i>
-                   </div>
-                   <div class="desc">
-                    <div class="title">
-                        <h4 >
-                            {{ mock('@word(10,30)') }}
-                        </h4>
-                        <div class="setting">
-                            <span style="margin-right: 20px;">0%</span>
-                            <i class="colourless bofangqi-zanting" title="暂停上传"></i>
-                            <i class="colourless shuayishua" title="重新上传"></i>
-                            <i class="colourless guanbi" title="取消上传"></i>
-                        </div>
+                <li v-for="index in 10">
+                    <div class="icon">
+                        <i class="colourless bofangshu"></i>
                     </div>
-                 <div class="jindu"></div>
-                   </div>
+                    <div class="desc">
+                        <div class="title">
+                            <h4>
+                                {{ mock('@word(10,30)') }}
+                            </h4>
+                            <div class="setting">
+                                <span style="margin-right: 20px;">0%</span>
+                                <i class="colourless bofangqi-zanting" title="暂停上传"></i>
+                                <i class="colourless shuayishua" title="重新上传"></i>
+                                <i class="colourless guanbi" title="取消上传"></i>
+                            </div>
+                        </div>
+                        <div class="jindu"></div>
+                    </div>
                 </li>
             </ul>
             <ul class="text">
-             <li>
-                <div class="title" style="background-color: chocolate;">标题</div>
-                <div class="content c1" style="background-color: cornflowerblue;">
-                <input type="text" placeholder="请输入标题">
-                <span >8/100</span>
+                <li>
+                    <div class="title" >标题</div>
+                    <div class="content c1" >
+                        <input type="text" placeholder="请输入标题">
+                        <span>8/100</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="title">类型</div>
+                    <div class="content c2" >
+                        <div>
+                            <input  type="radio" name="type"><span>自制</span>
+                        </div>
+                        <div class="zz">
+                            <input type="radio" name="type"><span style="margin-right: 10px;">转载</span>
+                            <input type="text" placeholder="转载请注明出处">
+                        </div>
+
+                    </div>
+                </li>
+                <li>
+                    <div class="title" >分区</div>
+                    <div class="content c3" >
+                       <select>
+                        <option selected>{{ mock('@cword(5,10)') }}</option>
+                        <option 
+                        v-for="index in 7"
+                        >{{ mock('@cword(5,20)') }}</option>
+                      
+                       </select>
+                    </div>
+                </li>
+                <li>
+                    <div class="title" style="align-self: flex-start;" >标签</div>
+                    <div class="content c4" >
+                     <div class="input">
+                            <input type="text" placeholder="按下回车键Enter添加标签">
+                            <span>还可以添加{{ mock('@integer(3,10)') }}个标签</span>
+                     </div>
+                     <div class="tag">
+                        <div class="t">
+                            <h4 >推荐标签</h4>
+                            <ul class="list">
+                                    <li
+                                    v-for="index in 10"
+                                    >
+                                    
+                                    {{ mock('@cword(2,5)') }}</li>
+                            </ul>
+                        </div>
+                        <div class="hua">
+                            <h4>推荐话题</h4>
+                            <ul class="hualist">
+                                    <li
+                                    v-for="index in 10"
+                                    >{{ mock('@cword(2,5)') }}</li>
+                            </ul>
+                        </div>
+                     </div>
+                    </div>
+                </li>
+                <li >
+                    <div class="title" style="align-self: flex-start;">简介</div>
+                    <div class="content c5" >
+                       <textarea  cols="30" rows="10" placeholder="输入视频简介"></textarea>
+                    </div>
+                </li>
+                <div class="submit">
+                    <button>立即投稿</button>
+                 
                 </div>
-             </li>
-             <li>
-                <div class="title" style="background-color: chocolate;">类型</div>
-                <div class="content c2" style="background-color: cornflowerblue;">
-             <div>
-                <input type="radio" name="type"><span>自制</span>
-             </div>
-             <div >
-                <input type="radio" name="type"><span>转载</span>
-                <input type="text" placeholder="装载请注明出处">
-             </div>
-             
-                </div>
-             </li>
-             <li>
-                <div class="title" style="background-color: chocolate;">标题</div>
-                <div class="content c1" style="background-color: cornflowerblue;">
-                <input type="text" placeholder="请输入标题">
-                <span >8/100</span>
-                </div>
-             </li>
             </ul>
         </div>
     </form>
@@ -143,7 +185,7 @@ const show3 = ref(false)
 <style scoped>
 .video_upload {
 
-    background-color: #fff;
+    /* background-color: #fff; */
     width: 826px;
 
     height: 266px;
@@ -173,7 +215,7 @@ const show3 = ref(false)
     width: 150px;
     border-radius: 8px;
     cursor: pointer;
-    background-color: #0aaee0;
+    /* background-color: #0aaee0; */
     position: relative;
     display: flex;
     align-items: center;
@@ -215,11 +257,11 @@ const show3 = ref(false)
     bottom: 20px;
     right: -50%;
     color: #000;
-    background-color: #fcfae0;
+    /* background-color: #fcfae0; */
 }
 
 form {
-    background-color: bisque;
+    /* background-color: bisque; */
     /* height: 100%; */
     width: 100%;
 }
@@ -240,7 +282,7 @@ form .header h3 {
 form .header .add {
     color: #fff;
     cursor: pointer;
-    background-color: #0aaee0;
+    /* background-color: #0aaee0; */
     padding: 3px 5px;
     border-radius: 6px;
 }
@@ -249,102 +291,203 @@ form .header .add {
     height: 100%;
     width: 100%;
     padding-left: 50px;
-    background-color: cadetblue;
+    /* background-color: cadetblue; */
 }
-.form .videos{
-    max-height: calc(60px * 3);
+
+.form .videos {
+    max-height: calc(60px * 5);
     overflow: auto;
 }
-.form .videos li{
-display: flex;
-width: 100%;
-background-color: darkgoldenrod;
-height: 60px;
-align-items: center;
-justify-content: flex-start;
+
+.form .videos li {
+    display: flex;
+    width: 100%;
+    /* background-color: darkgoldenrod; */
+    height: 60px;
+    align-items: center;
+    justify-content: flex-start;
 }
-.videos li .icon{
+
+.videos li .icon {
     margin-right: 20px;
 }
-.videos li .icon i{
+
+.videos li .icon i {
     color: #0aaee0;
     font-size: 50px;
 }
-.videos li .desc{
-padding-top: 5px;
-padding-bottom: 10px;
-padding-right: 20px;
+
+.videos li .desc {
+    padding-top: 5px;
+    padding-bottom: 10px;
+    padding-right: 20px;
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-   
+
 }
-.videos li .desc .title{
+
+.videos li .desc .title {
     display: flex;
     align-items: center;
     width: 100%;
     justify-content: space-between;
 }
-.videos li .desc .title h4{
+
+.videos li .desc .title h4 {
     font-weight: normal;
     font-size: 20px;
 }
-.setting i{
+
+.setting i {
     margin-left: 10px;
     font-size: 20px;
     background-color: #e6e7e8;
     cursor: pointer;
 }
-.jindu{
-   width: 100%;
-   border: 2px solid #e6e7e8;
- 
+
+.jindu {
+    width: 100%;
+    border: 2px solid #e6e7e8;
+
 }
-.text{
+
+.text {
     margin-top: 20px;
     width: 100%;
     /* height: 6000px; */
     height: 600px;
-    background-color: blueviolet;
+    /* background-color: blueviolet; */
 }
-.text li{
+
+.text li {
     margin-bottom: 10px;
     display: grid;
     width: 100%;
-    grid-template-columns: 200px  1fr;
+    grid-template-columns: 200px 1fr;
     justify-content: flex-start;
-align-items: center;
+    align-items: center;
 }
-.text li .title{
+
+.text li .title {
+    position: relative;
     font-size: 20px;
     font-weight: normal;
 }
 
-.text li .c1{
-height: 40px;
-width: 100%;
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 0 10px;
-}
-.c1 input{
-height: 80%;
-width: 90%;
-/* background: none; */
+.text li .c1 {
+    border: 2px solid #e6e7e8;
+    border-radius: 6px;
+    height: 40px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px;
 }
 
-.c2>div{
-    display: inline-block;
-margin-right: 20px;
-font-size: 20px;
-
+.c1 input {
+    height: 80%;
+    width: 90%;
+    /* background: none; */
 }
-.c2>div input[type='text']{
+.c2{
+    display: flex;
+    align-items: center;
+}
+.c2>div {
+   
+    margin-right: 20px;
+    font-size: 20px;
+color: #61666d;
+}
+
+.c2 .zz{
+    display: flex;
+   
+}
+.c2 .zz input[type='text']{
+    border: 1px solid #e6e7e8;
+    padding-left: 5px;
+}
+.c3 select{
+    padding: 5px 10px;
+    border-radius: 6px;
+    border: 2px solid #e6e7e8;
+}
+.c4 .input{
+    height: 30px;
+    width: 100%;
+    border: 2px  solid #e6e7e8;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.c4 .input input{
+    width: 80%;
+    height: 90%;
+}
+.c4 .input span{
+    color: #e6e7e8;
+}
+.c4 .tag{
+    /* background-color: aqua; */
+    margin-top: 20px;
+}
+.c4 .tag .t,
+.c4 .tag .hua
+
+{
+
+    margin-bottom: 10px;
+    display: grid;
+    grid-template-columns: 100px  auto;
+}
+.c4 .tag .t .list{
+    display: flex;
+    justify-content: flex-start;
+    /* background-color: blueviolet; */
+    flex-wrap: wrap;
+}
+.c4 .tag li{
+    background-color: #e6e7e8;
+}
+.c4 .tag .t .list li{
+   
+ cursor: pointer;
+ width: auto;
+ display: inline-block;
+ margin-right: 10px;
+ padding: 3px 5px;
+ border-radius: 4px;
+}
+
+.c4 .tag .hua .hualist{
+    display: flex;
+    justify-content: flex-start;
+    /* background-color: greenyellow; */
+    flex-wrap: wrap;
+}
+.c4 .tag .hua .hualist li{
+    cursor: pointer;
+    /* background-color: burlywood; */
  
-    margin-left: 10px;
-    height: 100%;
+ width: auto;
+ display: inline-block;
+ margin-right: 10px;
+ padding: 3px 5px;
+ border-radius: 4px;
+}
+.c5 textarea{
+    width: 80%;
+}
+.submit button{
+    padding: 10px;
+    background-color: #0aaee0;
+    color: #fff;
+    border: none;
+    border-radius: 6px;
 }
 </style>
