969a554f31cb358bde2dff79a5696e46be60455f
cwj <2735103940@qq.com>
2023-11-11 16:10:20 +0800

    重写弹幕

--- src/api/danmu.js
+++ src/api/danmu.js
@@ -4,14 +4,3 @@ export async function GetVideoListvideos(id) {
     return await axios.get(prevurl + '/VideoListvideos/' + id)
         .then(req => req.data)
 }
-//通知后台把视频弹幕全部放入redis
-export async function createVideoRedis(id) {
-    //初始化redis弹幕池
-    return await axios.get(prevurl + '/createVideoRedis/' + id)
-}
-
-export async function GetDanmuBymoment(id, start, end,uuid) {
-    // return []
-    return await axios.get(prevurl + `/DanmuBymoment/${id}/${start}/${end}/${uuid}`)
-        .then(req => req.data)
-}
